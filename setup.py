cc1cfc6805ed580a6e4488c18b8b395b3e02de26
jmcph4 <jmcph4@users.noreply.github.com>
2018-12-20 16:17:27 +1000

    Fix `pytest` version requirements
    This commit ensures `pytest` is at least 3.6.0, complying with the
    requirements `pytest-xdist` has.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import (
 
 extras_require = {
     'test': [
-        "pytest==3.3.2",
+        "pytest>=3.6.0",
         "pytest-xdist",
         "tox>=2.9.1,<3",
         "hypothesis==3.74.3",
